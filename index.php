8dad1bab4a3b1478f69b3ec50c3842f6ed9550cf
= <=>
2020-02-25 01:28:36 +0530

    index.php file

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="assets/style/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <span class="has-text-weight-bold is-size-4">BuyAndSell</span>
+      <!-- <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> -->
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<section>
+  <div class="hero is-fullheight is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Buyosell</h1>
+        <h2 class="subtitle">Buy and sell for BPHC</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="tile is-ancestor">
+    <div class="tile is-4 has-text-centered">
+      <div class="tile is-child box">
+        Hello
+      </div>
+    </div>
+    <div class="tile is-success has-text-centered">
+      <div class="tile is-child box">
+        Hello
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <fieldset>
+    <div class="field">
+      <label for="username">Username</label>
+      <div class="control">
+        <input class="input" type="text" name="username" value="">
+      </div>
+      <p class="help">Enter a username</p>
+    </div>
+    <div class="field has-addons">
+
+
+      <div class="control has-icons-left">
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+        <input class="input" type="email" name="email" value="">
+
+      </div>
+      <div class="control">
+        <button class="button is-static">@hyderabad.bits-pilani.ac.in</button>
+      </div>
+
+    </div>
+    <div class="field">
+      <label for="gender">Gender</label>
+      <div class="control">
+        <div class="select">
+          <select class="input">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+      </div>
+      <p class="help">Enter a email</p>
+    </div>
+  </div>
+  <div class="field">
+    <label class="checkbox">
+      <input type="checkbox" name="agree">
+      Do you agree
+    </label>
+  </div>
+
+</fieldset>
+</section>
+<section class="section">
+  <footer class="footer">
+    <div class="content has-text-centered">
+      &copy; 2020 BPHC  <strong>All Rights Reserved</strong>
+      <blockquote cite="http://">
+        Hello
+      </blockquote>
+    </div>
+  </footer>
+</section>
+  </body>
+</html>
+<!-- I don't think I will ever write the comments manually from now on  -->
